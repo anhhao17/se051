1107bb9c35555b99a7d30b02094fea97832c6f83
haonguyen <haonguyen@iritech.com>
2026-06-08 11:01:08 +0700

    mbedtls: define MBEDTLS_CMAC_C to align with SE05X

--- armhf-toolchain.cmake
+++ armhf-toolchain.cmake
@@ -1,8 +1,18 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_C_COMPILER   arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+# Root of the toolchain (use $ENV{HOME} instead of ~)
+set(TOOLCHAIN_ROOT $ENV{HOME}/work/iritech26tpro/tools/gcc-11.1.0-20210608-sigmastar-glibc-x86_64_arm-linux-gnueabihf)
+
+# The cross prefix used by all the tools
+set(TOOLCHAIN_PREFIX arm-linux-gnueabihf-)
+
+set(CMAKE_C_COMPILER   ${TOOLCHAIN_ROOT}/bin/${TOOLCHAIN_PREFIX}gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_ROOT}/bin/${TOOLCHAIN_PREFIX}g++)
+set(CMAKE_STRIP        ${TOOLCHAIN_ROOT}/bin/${TOOLCHAIN_PREFIX}strip)
+
+# Where to look for the target environment (sysroot)
+set(CMAKE_FIND_ROOT_PATH ${TOOLCHAIN_ROOT}/arm-linux-gnueabihf/libc)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
